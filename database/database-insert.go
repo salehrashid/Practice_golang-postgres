e69f711fbf1eef9309b049bf8be26a4bc4ad832c
salehrashid <saleh.rashid.b@gmail.com>
2023-09-20 17:42:43 +0700

    add constant

--- database/database-insert.go
+++ database/database-insert.go
@@ -4,12 +4,13 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq"
+	"go-postgres/constants"
 )
 
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+		constants.Host, constants.Port, constants.User, constants.Password, constants.Dbname)
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
