a5b7ea5358fd56b58a3a85d9626072db15e9c4ac
salehrashid <saleh.rashid.b@gmail.com>
2023-09-29 15:08:28 +0700

    code optimization and adjustment

--- gorm/database-web.go
+++ gorm/database-web.go
@@ -7,7 +7,6 @@ import (
 	"gorm.io/gorm"
 	"html/template"
 	"net/http"
-	"path"
 )
 
 type Cars struct {
@@ -22,43 +21,10 @@ func main() {
 	dbInsertRecords()
 }
 
-func index(writer http.ResponseWriter, _ *http.Request) {
-	dsn := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		constants.Host, constants.Port, constants.User, constants.Password, constants.Dbname)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
-	htmlFile := path.Join("gorm/template", "index.html")
-	tmplRender, err := template.ParseFiles(htmlFile)
-
-	if err != nil {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
-	}
-
-	var cars []Cars
-	db.Find(&cars)
-
-	if err := tmplRender.Execute(writer, cars); err != nil {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
-	}
-}
-
 func httpHandler() {
 	fmt.Println("server nya lagi jalan nih bang, http://localhost:7000")
 
-	//digunakan untuk routing aplikasi web.
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		root := path.Join("gorm/template", "root.html")
-		tmpl, err := template.ParseFiles(root)
-		if err != nil {
-			http.Error(writer, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		if err := tmpl.Execute(writer, tmpl); err != nil {
-			http.Error(writer, err.Error(), http.StatusInternalServerError)
-		}
-	})
-
+	http.HandleFunc("/", root)
 	http.HandleFunc("/index", index)
 
 	/**
@@ -66,9 +32,32 @@ func httpHandler() {
 	menggunakan server tersebut. Di Go, 1 web aplikasi adalah 1 buah
 	server berbeda.
 	*/
-	port := ":7000"
-	err := http.ListenAndServe(port, nil)
+	if err := http.ListenAndServe(":7000", nil); err != nil {
+		panic(err)
+	}
+}
+
+func root(writer http.ResponseWriter, _ *http.Request) {
+	tmplt := template.Must(template.ParseFiles("gorm/template/root.html"))
+	if err := tmplt.Execute(writer, nil); err != nil {
+		panic(err)
+	}
+}
+
+func index(writer http.ResponseWriter, _ *http.Request) {
+	dsn := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		constants.Host, constants.Port, constants.User, constants.Password, constants.Dbname)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
+		panic(err)
+	}
+
+	var cars []Cars
+	db.Find(&cars)
+
+	tmplt := template.Must(template.ParseFiles("gorm/template/index.html"))
+	if err := tmplt.Execute(writer, cars); err != nil {
 		panic(err)
 	}
 }
@@ -85,16 +74,16 @@ func dbInsertRecords() {
 	if err := db.AutoMigrate(&Cars{}); err != nil {
 		panic(err)
 	}
-	db.Create(&Cars{Name: "skyline r34", Engine: "RB26DETT twin-turbocharged 2.6-liter inline-six"})
-	db.Create(&Cars{Name: "dodge charger srt", Engine: "Supercharged 6.2L HEMI V8"})
-	db.Create(&Cars{Name: "supra mk4", Engine: "2JZ-GTE 3.0-litre twin-turbocharged straight 6"})
-	db.Create(&Cars{Name: "skyline r35", Engine: "VR38DETT V6 3.8 L twin-turbocharged"})
-	db.Create(&Cars{Name: "subaru wrx sti", Engine: "flat-four turbocharged 2.5-liter"})
-	db.Create(&Cars{Name: "shelby gt500", Engine: "5.2 L Predator V8"})
-	db.Create(&Cars{Name: "corvette z06 2006", Engine: "7.0 L LS7 V8"})
-	db.Create(&Cars{Name: "slivia s15", Engine: "2.0 L SR20DET I4 turbo"})
-	db.Create(&Cars{Name: "mazda rx7", Engine: "13B-REW twin-turbo twin-rotor"})
-	db.Create(&Cars{Name: "lexus lfa", Engine: "4.8 L 1LR-GUE V10"})
-	db.Create(&Cars{Name: "mitsubishi evo 9", Engine: "2.0 L 4G63 Inline 4 Turbocharged"})
-	db.Create(&Cars{Name: "370z fairlady", Engine: "3.7 L VQ37VHR V6"})
+	//db.Create(&Cars{Name: "skyline r34", Engine: "RB26DETT twin-turbocharged 2.6-liter inline-six"})
+	//db.Create(&Cars{Name: "dodge charger srt", Engine: "Supercharged 6.2L HEMI V8"})
+	//db.Create(&Cars{Name: "supra mk4", Engine: "2JZ-GTE 3.0-litre twin-turbocharged straight 6"})
+	//db.Create(&Cars{Name: "skyline r35", Engine: "VR38DETT V6 3.8 L twin-turbocharged"})
+	//db.Create(&Cars{Name: "subaru wrx sti", Engine: "flat-four turbocharged 2.5-liter"})
+	//db.Create(&Cars{Name: "shelby gt500", Engine: "5.2 L Predator V8"})
+	//db.Create(&Cars{Name: "corvette z06 2006", Engine: "7.0 L LS7 V8"})
+	//db.Create(&Cars{Name: "slivia s15", Engine: "2.0 L SR20DET I4 turbo"})
+	//db.Create(&Cars{Name: "mazda rx7", Engine: "13B-REW twin-turbo twin-rotor"})
+	//db.Create(&Cars{Name: "lexus lfa", Engine: "4.8 L 1LR-GUE V10"})
+	//db.Create(&Cars{Name: "mitsubishi evo 9", Engine: "2.0 L 4G63 Inline 4 Turbocharged"})
+	//db.Create(&Cars{Name: "370z fairlady", Engine: "3.7 L VQ37VHR V6"})
 }
